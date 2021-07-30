678afcfeeaa1bc5b984c42495a889e376a3657db
Muhammad Gusti Apriansyah <64070906+gustiafriansyah@users.noreply.github.com>
2021-07-30 09:19:19 +0700

    Upload Kontak
    Upload Kontak

---
+++ kontak.php
@@ -0,0 +1,240 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>SMK YAPEMRI Depok</title>
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+  <meta content="" name="keywords">
+  <meta content="" name="description">
+
+  <!-- Favicons -->
+  <link href="img/LOGOYAPEM.png" rel="icon">
+  <link href="img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,700,700i|Montserrat:300,400,500,700" rel="stylesheet">
+
+  <!-- Bootstrap CSS File -->
+  <link href="lib/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+  <!-- Libraries CSS Files -->
+  <link href="lib/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+  <link href="lib/animate/animate.min.css" rel="stylesheet">
+  <link href="lib/ionicons/css/ionicons.min.css" rel="stylesheet">
+  <link href="lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
+  <link href="lib/lightbox/css/lightbox.min.css" rel="stylesheet">
+
+  <!-- Main Stylesheet File -->
+  <link href="css/style.css" rel="stylesheet">
+
+  <!-- =======================================================
+    Theme Name: NewBiz
+    Theme URL: https://bootstrapmade.com/newbiz-bootstrap-business-template/
+    Author: BootstrapMade.com
+    License: https://bootstrapmade.com/license/
+  ======================================================= -->
+</head>
+
+<body>
+
+  <!--==========================
+  Header
+  ============================-->
+  <header id="header" class="fixed-top">
+    <div class="container">
+
+      <div class="logo float-left">
+        <!-- Uncomment below if you prefer to use an image logo -->
+        <!-- <h1 class="text-light"><a href="#header"><span>NewBiz</span></a></h1> -->
+        <a href="#intro" class="scrollto"><img src="img/logo1.png" alt="" class="img-fluid"></a>
+      </div>
+
+      <nav class="main-nav float-right d-none d-lg-block">
+        <ul>
+          <li class="active"><a href="index.html">Beranda</a></li>
+          <li><a href="index.html#profil">Profil</a></li>
+		  <li><a href="index.html#Galeri">Galeri</a></li>
+          <li><a href="#pendaftaran">Pendaftaran</a></li>
+          <li><a href="#berita">Berita</a></li>
+          <li><a href="kontak.php">Kontak</a></li>
+        </ul>
+      </nav><!-- .main-nav -->
+      
+    </div>
+  </header><!-- #header -->
+
+  <!--==========================
+    Intro Section
+  ============================-->
+  <br>
+  <br>
+
+  <main id="main">   
+
+   <!--==========================
+	Contact Section
+    ============================-->
+    <section id="contact">
+      <div class="container-fluid">
+
+        <div class="section-header">
+          <h3>Kontak</h3>
+        </div>
+
+        <div class="row wow fadeInUp">
+
+          <div class="col-lg-6">
+            <div class="map mb-4 mb-lg-0">
+			<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d3965.0014841655243!2d106.8529326!3d-6.3938088!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69eba402f4f695%3A0x4921f221fb56a766!2sSmk%20Yapemri!5e0!3m2!1sen!2sid!4v1573650527994!5m2!1sen!2sid" width="1000" height="600" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
+            </div>
+          </div>
+
+          <div class="col-lg-6">
+            <div class="row">
+              <div class="col-md-5 info">
+                <i class="ion-ios-location-outline"></i>
+                <p>Jl. Agung Ujung No.3, Depok</p>
+              </div>
+              <div class="col-md-4 info">
+                <i class="ion-ios-email-outline"></i>
+                <p>smkyapemdpk@gmail.com</p>
+              </div>
+              <div class="col-md-3 info">
+                <i class="ion-ios-telephone-outline"></i>
+                <p>021 7707489</p>
+              </div>
+            </div>
+
+            <div class="form">
+              <div id="sendmessage">Pesan Kamu Sudah Di Kirim Terimakasih </div>
+              <div id="errormessage"></div>
+              <form action="" method="post" role="form" class="contactForm">
+                <div class="form-row">
+                  <div class="form-group col-lg-6">
+                    <input type="text" name="name" class="form-control" id="name" placeholder="Nama" data-rule="minlen:4" data-msg="Mohon untuk masukkan nama minimal 4 huruf" />
+                    <div class="validation"></div>
+                  </div>
+                  <div class="form-group col-lg-6">
+                    <input type="email" class="form-control" name="email" id="email" placeholder="Email" data-rule="email" data-msg="Mohon masukkan email yang Valid" />
+                    <div class="validation"></div>
+                  </div>
+                </div>
+                <div class="form-group">
+                  <input type="text" class="form-control" name="subject" id="subject" placeholder="Subject" data-rule="minlen:4" data-msg="Mohon untuk memasukkan minimal 8 huruf" />
+                  <div class="validation"></div>
+                </div>
+                <div class="form-group">
+                  <textarea class="form-control" name="message" rows="5" data-rule="required" data-msg="Mohon untuk menulis sesuatu disini" placeholder="Isi Pesan"></textarea>
+                  <div class="validation"></div>
+                </div>
+                <div class="text-center"><button type="submit" title="Send Message">Kirim Pesan</button></div>
+              </form>
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+    </section><!-- #contact -->
+
+  </main>
+  
+    <!--==========================
+    Footer
+  ============================-->
+  <footer id="footer">
+    <div class="footer-top">
+      <div class="container">
+        <div class="row">
+
+          <div class="col-lg-4 col-md-6 footer-info">
+            <h3>SMK YAPEMRI</h3>
+            <p>Sekolah Menengah Keatas Terakreditasi A yang menjunjung tinggi pendidikan untuk siswa-siswa agar mempunyai masa depan yang lebih baik</p>
+          </div>
+
+          <div class="col-lg-2 col-md-6 footer-links">
+            <h4>Link Alternative</h4>
+            <ul>
+             
+              <li><a href="#profil">Profil</a></li>
+              <li><a href="#Galeri">Galeri</a></li>
+              <li><a href="#">Pendaftaran</a></li>
+              <li><a href="#">Berita</a></li>
+			  <li><a href="#">Kontak</a></li>
+            </ul>
+          </div>
+
+          <div class="col-lg-3 col-md-6 footer-contact">
+            <h4>Hubungi Ke</h4>
+            <p>
+              Jl. Agung Ujung Raya No.3<br>
+              Kec. Sukmajaya Depok II Timur<br>
+			  Kota Depok, Jawa Barat, 16417
+              Indonesia <br>
+              <strong>Telepon:</strong>021 7707489<br>
+              <strong>Email:</strong> smkyapemdpk@gmail.com<br>
+			  
+            </p>
+
+            <div class="social-links">
+              <a href="https://twitter.com/smkyapemri25" class="twitter"><i class="fa fa-twitter"></i></a>
+              <a href="https://www.facebook.com/groups/92388256043/" class="facebook"><i class="fa fa-facebook"></i></a>
+              <a href="https://instagram.com/yapemrischoll?igshid=145gbp0f6vnzu" class="instagram"><i class="fa fa-instagram"></i></a>
+              <a href="mailto:smkyapemdpk@gmail.com" class="google-plus"><i class="fa fa-google-plus"></i></a>
+              <a href="https://www.linkedin.com/in/gusti-afriansyah-ba288a195" class="linkedin"><i class="fa fa-linkedin"></i></a>
+            </div>
+
+          </div>
+
+          <div class="col-lg-3 col-md-6 footer-newsletter">
+            <h4>Informasi</h4>
+            <p>Masukkan email anda agar mendapatkan info-info seputar sekolah SMK Yapemri Depok	</p>
+            <form action="" method="post">
+              <input type="email" name="email"><input type="submit"  value="Subscribe">
+            </form>
+          </div>
+
+        </div>
+      </div>
+    </div>
+
+    <div class="container">
+      <div class="copyright">
+        &copy; Copyright <strong>NewBiz</strong>. All Rights Reserved
+      </div>
+      <div class="credits">
+        <!--
+          All the links in the footer should remain intact.
+          You can delete the links only if you purchased the pro version.
+          Licensing information: https://bootstrapmade.com/license/
+          Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/buy/?theme=NewBiz
+        -->
+        Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
+      </div>
+    </div>
+  </footer><!-- #footer -->
+
+  <a href="#" class="back-to-top"><i class="fa fa-chevron-up"></i></a>
+  <!-- Uncomment below i you want to use a preloader -->
+  <!-- <div id="preloader"></div> -->
+
+  <!-- JavaScript Libraries -->
+  <script src="lib/jquery/jquery.min.js"></script>
+  <script src="lib/jquery/jquery-migrate.min.js"></script>
+  <script src="lib/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="lib/easing/easing.min.js"></script>
+  <script src="lib/mobile-nav/mobile-nav.js"></script>
+  <script src="lib/wow/wow.min.js"></script>
+  <script src="lib/waypoints/waypoints.min.js"></script>
+  <script src="lib/counterup/counterup.min.js"></script>
+  <script src="lib/owlcarousel/owl.carousel.min.js"></script>
+  <script src="lib/isotope/isotope.pkgd.min.js"></script>
+  <script src="lib/lightbox/js/lightbox.min.js"></script>
+  <!-- Contact Form JavaScript File -->
+  <script src="contactform/contactform.js"></script>
+
+  <!-- Template Main Javascript File -->
+  <script src="js/main.js"></script>
+
+</body>
+</html>
